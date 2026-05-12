43438b58002ed3478ebb36ce7c5136cd5092c85d
Bui Quang Huy <huygialai2005@gmail.com>
2026-05-12 14:28:04 +0700

    Set up all file, make a connection to supabase

---
+++ utils/db.js
@@ -0,0 +1,24 @@
+import knex from 'knex';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT || 5432),
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    ssl: { rejectUnauthorized: false }, 
+  },
+  pool: { 
+    min: 2, 
+    max: 15,
+    propagateCreateError: false 
+  },
+  acquireConnectionTimeout: 15000,
+});
+
+export default db;
